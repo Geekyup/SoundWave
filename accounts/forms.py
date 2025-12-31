b8f5bdab5b39effef36728ae887cf1fd767e783c
Geekyup <laitovajan@mail.ru>
2025-12-31 14:18:32 +0300

    refactor(accounts): simplify model and forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
-from django.core.exceptions import ValidationError
+
 
 class ProfileForm(forms.ModelForm):
-    username = forms.CharField(max_length=150, required=True, label='Nickname')
+    username = forms.CharField(max_length=150, label='Nickname')
 
     class Meta:
         model = Profile
@@ -12,28 +12,18 @@ class ProfileForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if self.instance and getattr(self.instance, 'user', None):
-            self.fields['username'].initial = self.instance.user.username
+        self.fields['username'].initial = self.instance.user.username
 
     def clean_username(self):
         username = self.cleaned_data['username'].strip()
-        qs = User.objects.filter(username__iexact=username)
-        if self.instance and getattr(self.instance, 'user', None):
-            qs = qs.exclude(pk=self.instance.user.pk)
-        if qs.exists():
-            raise ValidationError('This username is already taken.')
+        if User.objects.filter(username__iexact=username).exclude(pk=self.instance.user.pk).exists():
+            raise forms.ValidationError('This username is already taken.')
         return username
 
     def save(self, commit=True):
         profile = super().save(commit=False)
-        username = self.cleaned_data.get('username')
-        if self.instance and getattr(self.instance, 'user', None) and username:
-            user = self.instance.user
-            user.username = username
-            if commit:
-                user.save()
+        profile.user.username = self.cleaned_data['username']
         if commit:
+            profile.user.save()
             profile.save()
-        else:
-            profile.user = self.instance.user
         return profile
--- accounts/models.py
+++ accounts/models.py
@@ -6,14 +6,14 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    avatar = models.ImageField(upload_to='avatars/%Y/%m/%d/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/%Y/%m/%d/', blank=True)
     bio = models.TextField(max_length=500, blank=True)
 
     def __str__(self):
-        return f'Profile: {self.user.username}'
+        return self.user.username
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
